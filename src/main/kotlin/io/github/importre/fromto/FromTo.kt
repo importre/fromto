e44eac58c278775886a4d58849d0848d2045f04a
Jaewe Heo <jaeweheo@gmail.com>
2015-12-14 01:42:16 +0900

    add FromTo.create(actions: List<FtAction<*>>)

--- src/main/kotlin/io/github/importre/fromto/FromTo.kt
+++ src/main/kotlin/io/github/importre/fromto/FromTo.kt
@@ -1,10 +1,15 @@
 package io.github.importre.fromto
 
-public class FromTo private constructor(val actions: Array<out FtAction<*>>) {
+public class FromTo private constructor(val actions: List<FtAction<*>>) {
 
     companion object {
         @JvmStatic
-        fun create(vararg actions: FtAction<*>): FromTo = FromTo(actions)
+        fun create(vararg actions: FtAction<*>): FromTo {
+            return FromTo(actions.toList())
+        }
+
+        @JvmStatic
+        fun create(actions: List<FtAction<*>>): FromTo = FromTo(actions)
     }
 
     var view: FtView? = null
